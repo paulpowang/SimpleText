2a8cf85fbb0a0bd99f73da1f241ec3a782234379
PoHung Wang <wang.po.hung@gmail.com>
2021-10-24 15:44:03 -0700

    add redux-devtools-extension dependency

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+const middleware = [];
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+    // other store enhancers if any
+  )
+);
 
 export default store;
