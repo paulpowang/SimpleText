450e9665159bb28355eb0b6f33ef754602dc7830
PoHung Wang <wang.po.hung@gmail.com>
2021-10-24 16:56:48 -0700

    add immutable dependency, and update Header

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,18 @@
+import { fromJS } from "immutable";
+
 import * as actionTypes from "./constants";
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false,
-};
+});
 
 // eslint-disable-next-line
 export default (state = defaultState, action) => {
   if (action.type === actionTypes.SEARCH_FOCUS) {
-    return {
-      focused: true,
-    };
+    return state.set("focused", true);
   }
   if (action.type === actionTypes.SEARCH_BLUR) {
-    return {
-      focused: false,
-    };
+    return state.set("focused", false);
   }
   return state;
 };
